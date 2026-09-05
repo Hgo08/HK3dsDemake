caedbd305bb4f531845047cf075a4e56dd09a7e2
Hgo08 <hugo.castbar@gmail.com>
2026-09-05 22:30:54 +0000

    refactor: textButton to be inherit from button

--- source/core/TextButton.cpp
+++ source/core/TextButton.cpp
@@ -1,77 +1,36 @@
 #include "TextButton.hpp"
 
-void TextButton::init(C2D_Font font, C2D_TextBuf textBuf, const std::string& label, float yPos, float textScale, std::function<void()> callback) {
-    y = yPos;
+void TextButton::init(C2D_Font font, C2D_TextBuf textBuf, const std::string& label, float xPos, float yPos, float textScale, float paddingX, float paddingY, std::function<void()> callback) {
     scale = textScale;
+    _paddingX = paddingX;
+    _paddingY = paddingY;
     onClick = callback;
 
     C2D_TextFontParse(&textObj, font, textBuf, label.c_str());
     C2D_TextOptimize(&textObj);
 
+    updateDimensions();
+    
+    y = yPos - (paddingY * scale * 0.5f);
     centerHorizontally(320.0f);
 }
 
-void TextButton::setPosition(float newX, float newY) {
-    x = newX;
-    y = newY;
+void TextButton::updateDimensions() {
+    width = (textObj.width * scale) + (_paddingX * scale);
+    height = (25.0f * scale) + (_paddingY * scale);
 }
 
 void TextButton::setPadding(float padX, float padY) {
-    paddingX = padX;
-    paddingY = padY;
-}
-
-void TextButton::centerHorizontally(float screenWidth) {
-    float textWidth = textObj.width * scale;
-    x = (screenWidth - textWidth) * 0.5f;
-}
-
-float TextButton::getRectX() const {
-    return x - (paddingX * scale * 0.5f);
-}
-
-float TextButton::getRectY() const {
-    return y - (paddingY * scale * 0.5f);
-}
-
-float TextButton::getRectWidth() const {
-    return (textObj.width * scale) + (paddingX * scale);
-}
-
-float TextButton::getRectHeight() const {
-    return (25.0f * scale) + (paddingY * scale);
-}
-
-bool TextButton::handleTouch(u32 kDown, const touchPosition& touch) {
-    if (!(kDown & KEY_TOUCH)) return false;
-
-    float rx = getRectX();
-    float ry = getRectY();
-    float rw = getRectWidth();
-    float rh = getRectHeight();
-
-    if (touch.px >= rx && touch.px <= rx + rw && touch.py >= ry && touch.py <= ry + rh) {
-        if (onClick) {
-            onClick();
-        }
-        return true;
-    }
-    return false;
+    _paddingX = padX;
+    _paddingY = padY;
+    updateDimensions();
 }
 
 void TextButton::render(bool drawDebugHitbox, int thickness, float depth) const {
-    C2D_DrawText(&textObj, C2D_WithColor, x, y, depth, scale, scale, textColor);
+    float textX = x + (_paddingX * scale * 0.5f);
+    float textY = y + (_paddingY * scale * 0.5f);
 
-    if (drawDebugHitbox) {
-        float rx = getRectX();
-        float ry = getRectY();
-        float rw = getRectWidth();
-        float rh = getRectHeight();
-        
-        C2D_DrawLine(rx,      ry,      outlineColor, rx + rw, ry,      outlineColor, thickness, depth);
-        C2D_DrawLine(rx + rw, ry,      outlineColor, rx + rw, ry + rh, outlineColor, thickness, depth);
-        C2D_DrawLine(rx + rw, ry + rh, outlineColor, rx,      ry + rh, outlineColor, thickness, depth);
-        C2D_DrawLine(rx,      ry + rh, outlineColor, rx,      ry,      outlineColor, thickness, depth);
-    }
+    C2D_DrawText(&textObj, C2D_WithColor, textX, textY, depth, scale, scale, textColor);
 
+    Button::render(drawDebugHitbox, thickness, depth);
 }
--- source/core/TextButton.hpp
+++ source/core/TextButton.hpp
@@ -1,35 +1,23 @@
 #pragma once
+#include "Button.hpp"
 #include <citro2d.h>
 #include <3ds.h>
-#include <functional>
 #include <string>
 
-class TextButton {
+class TextButton : Button{
 private:
     C2D_Text textObj;
-    float x = 0.0f;
-    float y = 0.0f;
     float scale = 1.0f;
-    float paddingX = 10.0f;
-    float paddingY = 10.0f;
+    float _paddingX = 10.0f;
+    float _paddingY = 10.0f;
     u32 textColor = C2D_Color32(255, 255, 255, 255);
-    u32 outlineColor = C2D_Color32(255, 0, 0, 255);
-    std::function<void()> onClick;
-
+    
+    void updateDimensions();
 public:
     TextButton() = default;
 
-    void init(C2D_Font font, C2D_TextBuf textBuf, const std::string& label, float yPos, float textScale = 0.5f, std::function<void()> callback = nullptr);
+    void init(C2D_Font font, C2D_TextBuf textBuf, const std::string& label, float xPos, float yPos, float textScale = 1, float paddingX = 10, float paddingY = 10, std::function<void()> callback = nullptr);
 
-    void centerHorizontally(float screenWidth = 320.0f);
-    void setPosition(float newX, float newY);
     void setPadding(float padX, float padY);
-
-    bool handleTouch(u32 kDown, const touchPosition& touch);
-    void render(bool drawDebugHitbox = false, int thickness = 1, float depth = 0.5) const;
-
-    float getRectX() const;
-    float getRectY() const;
-    float getRectWidth() const;
-    float getRectHeight() const;
+    void render(bool drawDebugHitbox = false, int thickness = 1, float depth = 0.5f) const override;
 };
